af767041e6bc76eed773ec744a927c5610154f73
harishtpj <harishtpj@hotmail.com>
2021-11-05 21:04:57 +0530

    Added List Commands

--- lib/fidis/list.rb
+++ lib/fidis/list.rb
@@ -12,3 +12,29 @@ def lset(key, lval)
     error "KeyValueError: Keys can only be Symbols", 705
   end
 end
+
+def lpush(key, lval)
+  if key.instance_of?(Symbol)
+    if (lval.instance_of?(Integer) || lval.instance_of?(Float) || lval.instance_of?(String))
+      $DB[key].prepend(lval)
+      700
+    else
+      error "KeyValueError: KeyValueError: Values only Support [Integer, Float, String]", 705
+    end
+  else
+    error "KeyValueError: Keys can only be Symbols", 705
+  end
+end
+
+def rpush(key, lval)
+  if key.instance_of?(Symbol)
+    if (lval.instance_of?(Integer) || lval.instance_of?(Float) || lval.instance_of?(String))
+      $DB[key].append(lval)
+      700
+    else
+      error "KeyValueError: KeyValueError: Values only Support [Integer, Float, String]", 705
+    end
+  else
+    error "KeyValueError: Keys can only be Symbols", 705
+  end
+end
